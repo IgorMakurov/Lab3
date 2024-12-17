0b89c1c189c6ad042d482cc67f598b0345cfd7d8
IgorMakurov <snegir2002@yandex.ru>
2024-12-17 12:37:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,52 @@
 #Минимальное число вывести прописью.
 
 digit_map = {
-    '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
-    '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
-}
-    
+        '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
+        '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
+    }
+
 count = 0
 min_number = float('inf')
 current_number = ""
+is_negative = False
+    
 
 with open('input.txt', 'r') as f:
     while True:
         char = f.read(1)
         if not char:
             break
+        if char == '-':
+            if not current_number:
+                is_negative = True
+            else:
+                if current_number and len(current_number) == 5:
+                    count += 1
+                    number_int = int(current_number)
+                    if is_negative:
+                        number_int = -number_int
+                    min_number = min(min_number, number_int)
+                is_negative = False
+                current_number = ""
 
-        if char.isdigit():
+        elif char.isdigit():
             current_number += char
+                
         elif current_number:
             if len(current_number) == 5:
                 count += 1
                 number_int = int(current_number)
+                if is_negative:
+                    number_int = -number_int
                 min_number = min(min_number, number_int)
+            is_negative = False
             current_number = ""
+
     if current_number and len(current_number) == 5:
         count += 1
         number_int = int(current_number)
+        if is_negative:
+            number_int = -number_int
         min_number = min(min_number, number_int)
 
 print(f"Количество 5-значных чисел: {count}")
@@ -41,8 +62,10 @@ print(f"Количество 5-значных чисел: {count}")
 if min_number != float('inf'):
     min_number_str = str(min_number)
     print("Минимальное число прописью (цифрами): ", end="")
-    for digit in min_number_str:
-        print( digit + ' ' + digit_map[digit], end=" ")
-    print()
+    if min_number_str[0] == '-':
+        print("- минус", end=" ")
+        min_number_str = min_number_str[1:]
+        for digit in min_number_str:
+            print(digit + ' ' + digit_map[digit], end=" ")
 else:
     print("Пятизначные числа в файле не найдены.")
